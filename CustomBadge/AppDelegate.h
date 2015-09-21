50c957f0ef4dab507653806f36837d68a5c0d85b
summer <zhu_guangwen@bingyan.net>
2015-09-21 19:18:37 +0800

    Initial Commit

---
+++ CustomBadge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomBadge
+//
+//  Created by ZhuGuangwen on 15/9/21.
+//  Copyright © 2015年 wepie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomBadge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomBadge
+//
+//  Created by ZhuGuangwen on 15/9/21.
+//  Copyright © 2015年 wepie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
